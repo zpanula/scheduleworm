fd7a4434004ca6cc6a70d2364f6e19e959616838
Zack Panula <1053951+zpanula@users.noreply.github.com>
2022-04-08 12:37:16 -0400

    Extract MongoDB connection

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import 'dotenv/config';
+import { connect } from './config/database.js';
 
+const connection = connect();
 const app = express();
 
 app.use(express.json());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-
-const mongoConnstring = process.env.MONGO_CONNSTRING || 'mongodb://localhost';
-
-mongoose
-  .connect(mongoConnstring)
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch((err) => console.error('Could not connect to MongoDB', err));
 
 app.get('/', (req, res) => {
   res.render('pages/index');
