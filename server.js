713fff369ddb900e0781d8a27ddf0362c2f283cd
Zack Panula <1053951+zpanula@users.noreply.github.com>
2022-04-10 15:30:50 -0400

    Add ESLint code styling rules

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-import express from "express";
-import mongoose from "mongoose";
+import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.use(express.json());
-app.use(express.static("public"));
-app.set("view engine", "ejs");
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
-const mongoConnstring = process.env.MONGO_CONNSTRING || "mongodb://localhost";
+const mongoConnstring = process.env.MONGO_CONNSTRING || 'mongodb://localhost';
 
 mongoose
   .connect(mongoConnstring)
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB", err));
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch((err) => console.error('Could not connect to MongoDB', err));
 
-app.get("/", (req, res) => {
-  res.render("pages/index");
+app.get('/', (req, res) => {
+  res.render('pages/index');
 });
 
 const port = process.env.PORT || 3000;
