c1f0462b0beabcee76ebaa7ae9fc92ab9fa7d549
Zack Panula <1053951+zpanula@users.noreply.github.com>
2022-04-10 23:30:06 -0400

    Add JWT authentication

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 import express from 'express';
-import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import users from './routes/users.js';
 import 'dotenv/config';
 import { connect } from './config/database.js';
 
 const connection = connect();
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+app.use('/user', users);
 
 app.get('/', (req, res) => {
   res.render('pages/index');
 });
 
-const { API_PORT } = process.env;
-const port = process.env.PORT || API_PORT;
+const port = process.env.API_PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
