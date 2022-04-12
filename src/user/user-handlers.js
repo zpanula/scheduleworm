33e93f057ce1707cd9f4742aff08e33d6b4dc888
Zack Panula <1053951+zpanula@users.noreply.github.com>
2022-04-12 15:10:31 -0400

    Add CRUD endpoints for user

--- src/user/user-handlers.js
+++ src/user/user-handlers.js
@@ -1,54 +1,34 @@
 import express from 'express';
-import { User, validate } from './user-model.js';
+import { validate } from './user-model.js';
 import auth from '../middleware/auth.js';
-import { read, create, login } from './user-service.js';
+import { read, remove, update } from './user-service.js';
 
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
+router.get('/:email', auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const { email, password } = req.body;
+  const user = await read(req.params.email);
+  if (!user) return res.status(400).send('User does not exist.');
 
-  const user = await read(email);
-  if (user) return res.status(400).send('User already registered.');
-
-  try {
-    await create({ email, password });
-  } catch (ex) {
-    console.log(ex.message);
-    res.status(500).send('Account creation failed.');
-  }
-  return res.send('Account successfully created.');
+  return res.send(user);
 });
 
-router.post('/login', async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.delete('/:email', auth, async (req, res) => {
+  const user = await read(req.params.email);
+  if (!user) return res.status(400).send('User does not exist.');
 
-  const user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send('Invalid email or password.');
-
-  try {
-    const token = login(req.body.email, req.body.password);
-
-    return res
-      .cookie('access_token', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-      })
-      .send('Logged in successfully.');
-  } catch (ex) {
-    return res.status(400).send('Invalid email or password.');
-  }
+  await remove(req.params.email);
+  return res.send('User deleted.');
 });
 
-router.get('/protected', auth, (req, res) =>
-  res.json({ user: { email: req.email } })
-);
+router.put('/:email', auth, async (req, res) => {
+  const user = await read(req.params.email);
+  if (!user) return res.status(400).send('User does not exist.');
 
-router.get('/logout', (req, res) =>
-  res.clearCookie('access_token').send('Successfully logged out.')
-);
+  await update(req.params.email, req.body.password);
+
+  return res.send('Password successfully changed.');
+});
 export default router;
--- src/user/user-service.js
+++ src/user/user-service.js
@@ -2,10 +2,14 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { User } from './user-model.js';
 
+async function generateHash(password) {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
+}
+
 export async function create(email, password) {
   const user = new User(email, password);
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
+  user.password = await generateHash(user.password);
 
   try {
     await user.save();
@@ -18,7 +22,7 @@ export async function create(email, password) {
 
 export async function read(email) {
   try {
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }).select('-password');
     return user;
   } catch (ex) {
     console.log(ex.message);
@@ -35,10 +39,24 @@ function generateAuthToken(user) {
 }
 
 export async function login(email, password) {
-  const user = await read(email);
+  const user = await User.findOne({ email });
 
   const validPassword = await bcrypt.compare(password, user.password); // TODO: Clarify variable names
   if (!validPassword) throw new Error('Invalid email or password.');
 
   return generateAuthToken(user);
+}
+
+export async function remove(email) {
+  const result = await User.deleteOne({ email });
+  return result;
+}
+
+export async function update(email, newPassword) {
+  await User.findOneAndUpdate(
+    { email },
+    {
+      password: await generateHash(newPassword),
+    }
+  );
 }
