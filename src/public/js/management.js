91c72850ab753fb969bbbd0b883ad37c97460d73
William Kopil <wkopcom@gmail.com>
2022-04-28 12:37:01 -0400

    created the forms for shift creations on user management.ejs

--- src/public/js/management.js
+++ src/public/js/management.js
@@ -1,23 +1,25 @@
-const adminPanel = document.getElementById('admin-panel');
-
-const actionMenu =
-  '<div class="dropdown"><button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Action</button><div class="dropdown-menu" aria-labelledby="dropdownMenuButton"><a class="dropdown-item" href="#">Action</a><a class="dropdown-item" href="#">Another action</a><a class="dropdown-item" href="#">Something else here</a></div></div>';
-
-fetch('/user', {
-  method: 'GET', // or 'PUT'
-  headers: {
-    'Content-Type': 'application/json',
+let newShift = {
+  username: '',
+  startTime: '',
+  endTime: '',
+  date: '',
+  clear() {
+    this.username = '';
+    this.startTime = '';
+    this.endTime = '';
+    this.date = '';
   },
-})
-  .then((response) => response.json())
-  .then((data) => {
-    adminPanel.innerHTML += '<tbody>';
-    data.forEach((element) => {
-      console.log(element.email);
-      adminPanel.innerHTML += `<tr><td>${element.email}</td><td>${element.isAdmin}</td><td>${actionMenu}</td></tr>`;
-      adminPanel.innerHTML += '</tbody>';
-    });
-  })
-  .catch((error) => {
-    throw new Error(error);
-  });
+  storeUser(username) {
+    this.username = username;
+  },
+};
+
+function updateNewShiftModalValues(username) {
+  document.getElementById('newShiftUsername').value = username;
+
+  const date = new Date();
+  document.getElementById('shiftDate').value = date
+    .toISOString()
+    .substring(0, 10);
+}
+
