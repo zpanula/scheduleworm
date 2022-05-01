a43c96266b15d8ee825d9bef35b29e278f87705f
Zack Panula <1053951+zpanula@users.noreply.github.com>
2022-05-01 15:40:35 -0400

    Remove unused function updateNewShiftModalValues

--- src/public/js/management.js
+++
@@ -1,9 +0,0 @@
-function updateNewShiftModalValues(username) {
-  document.getElementById('newShiftUsername').value = username;
-
-  const date = new Date();
-  document.getElementById('shiftDate').value = date
-    .toISOString()
-    .substring(0, 10);
-}
-
